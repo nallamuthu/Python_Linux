f895b133b38c9612be77df92b5151487fc0ecadf
n@ll@muthu <nallamuthu.92@gmail.com>
2020-04-09 21:33:14 +0400

    Update nmap_xml_parser.py

--- Parser/nmap_xml_parser.py
+++ Parser/nmap_xml_parser.py
@@ -6,10 +6,9 @@ def parse_nmap_xml_file(input_file):
 	    col_ordered_dict = xmltodict.parse(fd.read())
 	host_status=col_ordered_dict['nmaprun']['runstats']['hosts']['@up'] #Get the Host status Valye 1 or 0
 	if host_status=="1": #if Host status is up then process further
-		#Loop Through all the open ports and get the list
-		for items in col_ordered_dict['nmaprun']['host']['ports']['port']:
+		for items in col_ordered_dict['nmaprun']['host']['ports']['port']: #Loop Through all the open ports and get the list
 			if items['state']['@state']=="open":
-				open_ports_service[items['@portid']] = items['service']['@name']
+				open_ports_service[items['@portid']] = items['service']['@name'] #Save Port Number and Service Name into Dict
 	return host_status,open_ports_service
 	
 
