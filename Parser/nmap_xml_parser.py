b3f82daebe569a131b85ce30b078666674250f59
n@ll@muthu <nallamuthu.92@gmail.com>
2020-04-10 12:43:46 +0400

    SSLYZE Parser

--- Parser/nmap_xml_parser.py
+++ Parser/nmap_xml_parser.py
@@ -1,17 +1,23 @@
+#Read ME - NMAP Output file is converted to dictonary and processed
+#Input - NMAP Output XML File
+#Output - Dictonary (Contains the opened port number and service name)
+
 import xmltodict
 
 def parse_nmap_xml_file(input_file):
-	open_ports_service={}
+    result_dict={}
 	with open(input_file) as fd:
 	    col_ordered_dict = xmltodict.parse(fd.read())
 	host_status=col_ordered_dict['nmaprun']['runstats']['hosts']['@up'] #Get the Host status Valye 1 or 0
 	if host_status=="1": #if Host status is up then process further
 		for items in col_ordered_dict['nmaprun']['host']['ports']['port']: #Loop Through all the open ports and get the list
 			if items['state']['@state']=="open":
-				open_ports_service[items['@portid']] = items['service']['@name'] #Save Port Number and Service Name into Dict
-	return host_status,open_ports_service
+				result_dict[items['@portid']] = items['service']['@name'] #Save Port Number and Service Name into Dict
+	return host_status,result_dict
 	
 
-host_status,open_ports_service=parse_nmap_xml_file('output.xml')
+host_status,result_dict=parse_nmap_xml_file('output.xml')
 print("Host Status: "+host_status)
-print("Ports->Service: "+open_ports_service)
+print("Ports->Service: "+result_dict)
+
+
