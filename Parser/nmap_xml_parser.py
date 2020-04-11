07203f31b097eb53e65870d8394e5d767e4eb8da
n@ll@muthu <nallamuthu.92@gmail.com>
2020-04-11 16:10:29 +0400

    Update nmap_xml_parser.py

--- Parser/nmap_xml_parser.py
+++ Parser/nmap_xml_parser.py
@@ -1,22 +1,23 @@
 #Read ME - NMAP Output file is converted to dictonary and processed
 #Input - NMAP Output XML File
 #Output - Dictonary (Contains the opened port number and service name)
-
 import xmltodict
 
+#Function to parse the xml file
 def parse_nmap_xml_file(input_file):
-    result_dict={}
+    result_dict={} #To store the port_number:service_name
 	with open(input_file) as fd:
-	    col_ordered_dict = xmltodict.parse(fd.read())
+	    col_ordered_dict = xmltodict.parse(fd.read()) #Convert the xml file to dictonary to process it
 	host_status=col_ordered_dict['nmaprun']['runstats']['hosts']['@up'] #Get the Host status Valye 1 or 0
 	if host_status=="1": #if Host status is up then process further
 		for items in col_ordered_dict['nmaprun']['host']['ports']['port']: #Loop Through all the open ports and get the list
 			if items['state']['@state']=="open":
 				result_dict[items['@portid']] = items['service']['@name'] #Save Port Number and Service Name into Dict
 	return host_status,result_dict
-	
 
-host_status,result_dict=parse_nmap_xml_file('output.xml')
+
+input_file='output.xml'
+host_status,result_dict=parse_nmap_xml_file(input_file) #Call the function with the filename as parameter
 print("Host Status: "+host_status)
 print("Ports->Service: "+result_dict)
 
