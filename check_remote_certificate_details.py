7a25c3c2313b628a4a64a1c5d637ec5cce9ddf8c
n@ll@muthu <nallamuthu.92@gmail.com>
2020-04-11 17:38:29 +0400

    Update check_remote_certificate_details.py

--- check_remote_certificate_details.py
+++ check_remote_certificate_details.py
@@ -8,19 +8,23 @@ from datetime import timedelta, date
 
 #Get Certificate Details
 def get_cert_details(hostname,port):
-	result_dict={}
-	public_certificate=ssl.get_server_certificate((hostname, port))
-	x509_object = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, public_certificate)
-	exp_date =x509_object.get_notAfter()
-	exp_day = int(exp_date[6:8].decode('utf-8'))
-	exp_month = int(exp_date[4:6].decode('utf-8'))
-	exp_year = int(exp_date[:4].decode('utf-8'))
-	exp_date = str(exp_year) + "-" + str(exp_month) + "-" + str(exp_day)
-	result_dict['expiry_status']=x509_object.has_expired() #Certificate Expired or not
-	result_dict['expiry_date']=exp_date #The Date in which the certificate Expires
-	result_dict['expiry_in']=(date(exp_year,exp_month,exp_day)-date.today()).days #Get the No.of.Days Left for Expiry
-	result_dict['sign_alg']=(x509_object.get_signature_algorithm()).decode('utf-8') #Get the signature algorithm
-	return result_dict
+	result_dict={'expiry_status':'N/A','expiry_date':'N/A','expiry_in':'N/A','sign_alg':'N/A'}
+	try:
+		public_cert_obj=ssl.get_server_certificate((hostname, port))
+		x509_object = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, public_cert_obj)
+		exp_date =x509_object.get_notAfter()
+		exp_day = int(exp_date[6:8].decode('utf-8'))
+		exp_month = int(exp_date[4:6].decode('utf-8'))
+		exp_year = int(exp_date[:4].decode('utf-8'))
+		exp_date = str(exp_year) + "-" + str(exp_month) + "-" + str(exp_day)
+		result_dict['expiry_status']=x509_object.has_expired() #Certificate Expired or not
+		result_dict['expiry_date']=exp_date #The Date in which the certificate Expires
+		result_dict['expiry_in']=(date(exp_year,exp_month,exp_day)-date.today()).days #Get the No.of.Days Left for Expiry
+		result_dict['sign_alg']=(x509_object.get_signature_algorithm()).decode('utf-8') #Get the signature algorithm
+	except Exception as e:
+		#print(e)
+		result_dict = dict.fromkeys( result_dict, "ERROR" ) #if any error occurs set all the value to ERROR
+	return result_dict 	#Return the dict contains the result
 
 
 #hostname="expired.badssl.com"
@@ -31,3 +35,5 @@ port = 443
 #Function call to get the details
 result_dict=get_cert_details(hostname,port)
 print(result_dict)
+
+#Pending timeout, try catch
