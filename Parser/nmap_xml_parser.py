9eff520443322ab9bed41c22172ceca553252bda
n@ll@muthu <nallamuthu.92@gmail.com>
2020-04-11 17:42:05 +0400

    Update nmap_xml_parser.py

--- Parser/nmap_xml_parser.py
+++ Parser/nmap_xml_parser.py
@@ -8,14 +8,18 @@ import os
 def parse_nmap_xml_file(input_file):
 	result_dict={} #To store the port_number:service_name
 	host_status="N/A"
-	if os.path.isfile(input_file):  #Check if the file exist
-		with open(input_file) as fd:
-			col_ordered_dict = xmltodict.parse(fd.read()) #Convert the xml file to dictonary to process it
-		host_status=col_ordered_dict['nmaprun']['runstats']['hosts']['@up'] #Get the Host status Valye 1 or 0
-		if host_status=="1": #if Host status is up then process further
-			for items in col_ordered_dict['nmaprun']['host']['ports']['port']: #Loop Through all the open ports and get the list
-				if items['state']['@state']=="open":
-					result_dict[items['@portid']] = items['service']['@name'] #Save Port Number and Service Name into Dict
+	try:
+		if os.path.isfile(input_file):  #Check if the file exist
+			with open(input_file) as fd:
+				col_ordered_dict = xmltodict.parse(fd.read()) #Convert the xml file to dictonary to process it
+			host_status=col_ordered_dict['nmaprun']['runstats']['hosts']['@up'] #Get the Host status Valye 1 or 0
+			if host_status=="1": #if Host status is up then process further
+				for items in col_ordered_dict['nmaprun']['host']['ports']['port']: #Loop Through all the open ports and get the list
+					if items['state']['@state']=="open":
+						result_dict[items['@portid']] = items['service']['@name'] #Save Port Number and Service Name into Dict
+	except Exception as e:
+		#print(e)
+		host_status="ERROR"
 	return host_status,result_dict
 
 
