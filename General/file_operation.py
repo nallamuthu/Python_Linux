294afd1f5aca1994db6ca81b6a31c5a2d305e937
n@ll@muthu <nallamuthu.92@gmail.com>
2020-04-26 11:36:07 +0400

    Update file_operation.py

--- General/file_operation.py
+++ General/file_operation.py
@@ -1,3 +1,6 @@
+import os
+
+
 #Open file and replace particular string and save the file in same name
 def file_string_replace(input_file):
 	f1=open(input_file,"r+")
@@ -8,5 +11,8 @@ def file_string_replace(input_file):
 	f1.close()
 	f2.close()
     
-    
-file_string_replace('inp_file.txt')
+
+input_file='inp_file.txt'
+#Check if file exist
+if os.path.isfile(output_file):
+    file_string_replace(output_file)
