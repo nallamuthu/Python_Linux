4173abded71210d2601575afcfd0c3ccf59bd062
n@ll@muthu <nallamuthu.92@gmail.com>
2020-04-26 11:56:06 +0400

    Update list_opreation.py

--- General/list_opreation.py
+++ General/list_opreation.py
@@ -1,11 +1,21 @@
+list1=['one','two','three']
+list2=['three','four','five']
+
+#Merge two list
+merged_result=list1+list2
+
 #Convert File to List - each line will be treated as 1 elemtent in list
 file_list = [line.rstrip('\n') for line in open("filename.txt")]
+
 #Remove all the duplicate element from the list
 file_list = list(set(file_list))
+
 #Remove the empty elements/'' from the list
 file_list = [x for x in file_list if x] 
+
 #Total elements count in list
 list_count=len(file_list)
+
 #Search element exist in List
 if "search_text" in file_list: 
 	print("Element Found")
